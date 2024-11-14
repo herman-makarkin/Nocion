2841097c8bacb66d4d33ca420ab7745f0984934e
Herman <german.makarkin@gmail.com>
2024-11-14 11:05:47 +0000

    fix docker

--- infra/pgsql/script/initdb.sh
+++ infra/pgsql/script/initdb.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 
 function create_user_and_db() {
-  local database=$1
+    local database=$1
 
     echo "  Creating user and database '$database'"
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
@@ -13,11 +13,3 @@ function create_user_and_db() {
         GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
 EOSQL
 }
-
-if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
-    echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
-    for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-        create_user_and_db $db
-    done
-    echo "Multiple databases created"
-fi
